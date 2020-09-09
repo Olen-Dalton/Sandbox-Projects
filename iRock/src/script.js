60dafad35e7cbbe37d69d85829d2d8e11c32deca
Olen Dalton <45123657+Olen-Dalton@users.noreply.github.com>
2020-09-09 13:14:36 -0400

    Add files via upload

---
+++ iRock/src/script.js
@@ -0,0 +1,8 @@
+ function touchRock () {
+  var userName = prompt("What is your name?", "Enter your name here.");
+  
+  if (userName) {
+    alert("It's nice to meet you, " + userName + ".");
+    document.getElementById("rockImg") .src = "https://resources.oreilly.com/examples/9780596527747/raw/master/JS%20examples/chapter01/irock/rock_happy.png";
+  }
+}   
